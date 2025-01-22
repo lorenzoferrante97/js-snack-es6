072f5826b504bda1f49f105ff094de1db16046d8
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-22 15:12:38 +0100

    bonus: start bonus

--- main.js
+++ main.js
@@ -101,3 +101,5 @@ console.log("squadre aggiornate: ", squadreUpdate)
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array ( esempio: ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'] )e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con li elementi estratti dall'array di partenza che hanno la posizione compresa tra i due numeri (a e b).
+
+// start bonus
